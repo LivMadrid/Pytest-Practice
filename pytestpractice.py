79efe60c034fb3af67fe7fd3c12e6ed40240af27
Olivia <liv.madrid14@gmail.com>
2021-04-05 22:37:36 +0000

    Add Raise TypeError helper function. test_capital_case passes

---
+++ pytestpractice.py
@@ -0,0 +1,18 @@
+#practice test functions
+
+# x = "ciao"
+
+# assert function #if condition returns True - nothing happens
+
+# assert x == "ciao"
+## if false, AssertionError is thrown
+# assert x == "a presto"
+
+
+
+
+def func(x):
+    return x + 1 
+
+def test_answer():
+    assert func(3) == 5 
--- test_capitalize.py
+++ test_capitalize.py
@@ -1,7 +1,16 @@
 #sample basic pytests from semaphoreci.com pytest tutorials
+import pytest
+
 
 def capital_case(x):
+    if not isinstance(x, str):
+        raise TypeError('Please provide a string arg')
     return x.capitalize()
 
 def test_capital_case():
     assert capital_case('ciao') == 'Ciao'
+
+def test_raises_exception_on_non_string_arguments():
+    with pytest.raises(TypeError):
+        capital_case(9)
+
