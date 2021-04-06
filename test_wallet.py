766f30574ca493cb8b8f6417702eda00e795a784
Olivia <liv.madrid14@gmail.com>
2021-04-06 00:11:41 +0000

    Refactor tests again using parametrize and test fixture functions combined

--- test_wallet.py
+++ test_wallet.py
@@ -1,12 +1,32 @@
 #pytest test practice -example from semaphoreci.com
+#best practices/pointers for fixtures: use docstrings/ each test has its own new initialized class instance: Wallet() not one used in another test
+# $pytest --fixtures allows you to view all available fixtures
 
 import pytest
 
 
 from wallet import Wallet, InsufficientAmount
 
-#refactoring of tests to use the @pytest.fixture helper code 
+#refactoring of tests to use the pytest fixture helper code 
+# fixture functions are created by the decorator : @pytest.fixture
 # reduces boilerplate code of instance 'wallet = Wallet(10) etc.'
+
+#empty_wallet and wallet fixtures below initialize the Wallet() class in tests,
+#where it is needed with different values
+
+#parametrized test functions: test various combinations of these methods vs individual methods
+#the pytest.mark.parametrize will run for each set of parameters earned/spent/expected 
+
+@pytest.mark.parametrize('earned, spent, expected', [
+    (30, 10, 20), 
+    (20, 2, 18),
+    ])
+
+def test_transactions(earned, spent, expected): 
+    my_wallet = Wallet()
+    my_wallet.add_cash(earned)
+    my_wallet.spend_cash(spent)
+    assert my_wallet.balance == expected
 
 @pytest.fixture
 def empty_wallet():
@@ -46,3 +66,20 @@ def test_wallet_spend_cash_raises_exception_insufficient_amount(empty_wallet):
 
 
 
+#combining test fixtues and parametrized test functions
+#to refactor even more we can replace the wallet initialization with a test fixture
+
+@pytest.fixture
+def my_wallet():
+    '''Returns a Wallet instance with a zero balance '''
+    return Wallet()
+
+@pytest.mark.parametrize('earned, spent, expected', [
+    (30, 10, 20), 
+    (20, 2, 18),
+    ])
+
+def test_transactions(my_wallet, earned, spent, expected): 
+    my_wallet.add_cash(earned)
+    my_wallet.spend_cash(spent)
+    assert my_wallet.balance == expected
